55660094af36af981b26c7e2c685e720d7b88077
Santiago Pellejero <santiago.pellejero@correo.ucu.edu.uy>
2024-08-21 23:02:26 -0300

    Mesa

--- src/Table.cs
+++ src/Table.cs
@@ -7,9 +7,51 @@ using System.Collections;
 /// </summary>
 public class Table
 {
+    private int number;
+
+    public int Number
+    {
+        get { return number; } set {
+            
+            number = value;
+            
+        }
+    }
+    private bool isOccupied;
+
+    public bool IsOccupied
+    {
+        get { return isOccupied; } set {
+            
+            isOccupied = value;
+            
+        }
+    }
     private ArrayList order = new ArrayList();
+
+    public Table (int number)
+    {
+        this.number = number;
+        
+    }
     public bool HasOrders()
     {
         return this.order.Count > 0;
+    }
+
+    public void Ocupy()
+    {
+        this.isOccupied = true;
+    }
+
+    public void Free()
+    {
+        this.isOccupied = false;
+        this.order.Clear();
+    }
+
+    public void AddToOrder(Dish dish)
+    {
+        this.order.Add(dish);
     }
 }
