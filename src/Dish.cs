950153322dc02d9d78643f53c0a21be43158b5d5
Juan Ignacio Weyrauch <juan.weyrauch@correo.ucu.edu.uy>
2024-08-26 20:00:02 -0300

    Cambios en dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -6,4 +6,13 @@ namespace Ucu.Poo.Restaurant;
 public class Dish
 {
     private string Name { get; set; }
+    private int Price { get; set; }
+    private bool IsVegetarian { get; set; }
+
+    public Dish(string nombre, int precio, bool EsVegetariano)
+    {
+        this.Name = nombre;
+        this.Price = precio;
+        this.IsVegetarian = EsVegetariano;
+    }
 }
