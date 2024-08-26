bee0d16b3d291e02fdc6cab46071a25ca989e95b
Jesus Dos Santos <jesus.dossantos@correo.ucu.edu.uy>
2024-08-26 20:50:30 -0300

    menu1

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,3 +1,6 @@
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
 namespace Ucu.Poo.Restaurant;
 
 using System.Collections;
@@ -8,5 +11,41 @@ using System.Collections;
 /// </summary>
 public class Menu
 {
+    private string nombre { get; set };
+    private int precio { get; set };
+    private bool esVegetariano;
     private ArrayList dishes = new ArrayList();
+
+    public Menu(string unNombre, int unPrecio, bool unEsVegetariano)
+    {
+        this.nombre = unNombre;
+        this.precio = unPrecio;
+        this.esVegetariano = unEsVegetariano;
+    }
+
+    public void AddDish(Dish dish)
+    {
+        dishes.Add(dish);
+    }
+
+
+    public void RemoveDish(Dish dish)
+    {
+        dishes.Clear(dish);
+    }
+
+    public Dish GetDishByName(string nombre)
+    {
+        foreach (var dish in dishes)
+        {
+            if (dish.Nombre == nombre)
+            {
+                return dish;
+            }
+        }
+
+        Console.WriteLine("Dish not found");
+        return null;
+    }
+
 }
